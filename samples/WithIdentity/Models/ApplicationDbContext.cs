36f97a8228071cd73fa40fd0cb188b5dbd450d15
Mohammad Rahhal <m.r992@hotmail.com>
2016-04-19 20:17:10 +0300

    * typo

--- samples/WithIdentity/Models/ApplicationDbContext.cs
+++ samples/WithIdentity/Models/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ namespace WithIdentity.Models
 {
 	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 	{
-		// MNOTE: This will only ever be used by when calling the migrator tool (so only in production).
+		// MNOTE: This hard coded string will only be used when calling the migrator tool (so only in development).
 		public static string ConnectionString { get; set; } =
 			"Server=(localdb)\\mssqllocaldb;Database=aspnet5-WithIdentity-8167e428-d8a7-4cae-931f-6899730e0ae8;Trusted_Connection=True;MultipleActiveResultSets=true";
 
