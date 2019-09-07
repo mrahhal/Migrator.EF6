d6e3bfde29878b82a486a076acc5750872e20d1e
Mohammad Rahhal <m.r992@hotmail.com>
2019-09-07 16:22:25 +0300

    Create ResXResourceWriter.cs

---
+++ src/Migrator.EF6.Tools/ResXResourceWriter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Migrator.EF6.Tools
+{
+	public class ResXResourceWriter : IDisposable
+	{
+		private readonly string _path;
+		private readonly Dictionary<string, string> _resources = new Dictionary<string, string>();
+
+		public ResXResourceWriter(string path)
+		{
+			_path = path;
+		}
+
+		public void AddResource(string key, string value)
+		{
+			_resources.Add(key, value);
+		}
+
+		public void Dispose()
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			var fileContent = default(string);
+
+			using (var resourceStream = assembly.GetManifestResourceStream("Migrator.EF6.Tools.ResourceTemplate.txt"))
+			using (var reader = new StreamReader(resourceStream))
+			{
+				fileContent = reader.ReadToEnd();
+			}
+
+			var text = "";
+
+			foreach (var r in _resources)
+			{
+				text += $@"<data name=""{r.Key}"" xml:space=""preserve"">
+    <value>{r.Value}</value>
+  </data>\r\n";
+			}
+
+			fileContent = fileContent.Replace("{{PLACEHOLDER}}", text);
+
+			File.WriteAllText(_path, fileContent);
+		}
+	}
+}
